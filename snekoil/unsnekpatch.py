724f431eda1b4d52a225ee446f24ed9bbcff9a78
overflowerror <mail@overflowerror.com>
2024-01-19 23:30:43 +0100

    feat: Add snekpatching for list and filter (draft)

--- snekoil/unsnekpatch.py
+++ snekoil/unsnekpatch.py
@@ -2,10 +2,12 @@ import builtins
 import sys
 import time
 
-from snekoil.iterators import _original_range
+from snekoil.iterators import _original_filter, _original_list, _original_range
 from snekoil.time import _original_sleep
 
 builtins.range = _original_range
+builtins.list = _original_list
+builtins.filter = _original_filter
 time.sleep = _original_sleep
 
 # Remove the snekpatch module from sys.modules so
--- tests/test_snekpatch.py
+++ tests/test_snekpatch.py
@@ -1,21 +1,24 @@
+import random
 import time
 
 
-def test_snekpatch_range():
+def test_snekpatch_iterators():
+    random.seed(1337)
+
     # Test before snekpatch
-    assert list(range(10)) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert list(filter(None, range(10))) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     # Snekpatch
     import snekoil.snekpatch  # noqa (unused import)
 
     # Test after snekpatch
-    assert list(range(10)) == [4, 5, 6, 7, 8, 9]
+    assert list(filter(None, range(10))) == [4, 5, 6, 7, 8, 9]
 
     # Unsnekpatch
     import snekoil.unsnekpatch  # noqa (unused import)
 
     # Test after unsnekpatch
-    assert list(range(10)) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert list(filter(None, range(10))) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 def test_snekpatch_sleep():
